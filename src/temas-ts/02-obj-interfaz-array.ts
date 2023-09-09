e79fcdf49459c12b862cc31703d1f0e4cd72ce3c
IEVN-1002-20001036 <144471214+IEVN-1002-20001036@users.noreply.github.com>
2023-09-09 10:26:15 -0600

    09/09/2023

---
+++ src/temas-ts/02-obj-interfaz-array.ts
@@ -0,0 +1,34 @@
+
+let nombres: string[] = ['Mario', 'Luigi', 'Peach']
+  
+nombres.push('Toad')
+// nombres.push(100) -> error type
+console.log(nombres);
+
+let datos:(string | boolean | number)[]= ['Mario',23,true,'Lopez']
+datos.push(200)
+datos.push('Carlos')
+datos.unshift(false)
+console.log(datos);
+
+// otra forma de declarar arreglos 
+let arreglo: Array<number>=[1,2,3,4]
+
+// type Alumno = {
+  interface Alumno {
+  matricula: string,
+  nombre: string,
+  edad: number,
+  mail: string,
+  materias ?: string[]
+}
+
+const alumno: Alumno = {
+  matricula :  '20002340',
+  nombre: 'Danna',
+  edad: 22,
+  mail: 'danna@gmail.com'
+}
+
+alumno.nombre = 'paulina'
+console.log(alumno);
